1ab97cea0ab8098e931a1abc6b065860789d7a4c
Madoka Doi <doi@cloverlab.jp>
2014-02-20 21:21:33 +0900

    吉村くんの遺産

--- application/controllers/welcome2.php
+++ application/controllers/welcome2.php
@@ -2,14 +2,24 @@
 
 class Welcome2 extends CI_Controller {
 
+    function __construct(){
+        parent::__construct();
+        $this->load->database();
+    }
+
     public function index()
     {
-
+        $this->load->database();
         $this->load->model('T_tests');
         $test_text = $this->T_tests->get_text();
         $test_count = $this->T_tests->get_count();
-
+        var_dump($test_count);
         $this->view['_'] = array('message' => $test_text);
         $this->load->view('welcome2', $this->view);
     }
+}
+
+
+function __destruct(){
+    $this->db->close();
 }
--- application/models/t_tests.php
+++ application/models/t_tests.php
@@ -2,15 +2,18 @@
 class T_tests extends CI_Model
 {
 
-    //$this->load->database();
-
     public function get_text()
     {
         return "はろー！はろー！はろーわーるど！";
     }
 
     public function get_count() {
-        return 0;
+        $sql = "SELECT * FROM t_tests";
+
+        $params = array();
+        $res = $this->db->query($sql, $params)->result();
+
+        return $res;
     }
 
 }
